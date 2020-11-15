fe50574fd0bef58870fdc44fdbfb6b1f3cb0e805
Mark Veltzer <mark.veltzer@gmail.com>
2020-11-15 21:10:16 +0200

    better config names

--- pygooglehelper/configs.py
+++ pygooglehelper/configs.py
@@ -17,17 +17,17 @@ class ConfigAuth(Config):
     )
     # parameters passed to run_local_server
     host = ParamCreator.create_str(
-        help_string="The hostname for the local redirect server. This will be served over http, not https.",
+        help_string="The hostname for the local redirect server. This will be served over http, not https",
         default='localhost',
     )
     port = ParamCreator.create_int(
-        help_string="The port for the local redirect server.",
+        help_string="The port for the local redirect server",
         # default=8080,
         default=0,
     )
     # noinspection PyProtectedMember
     authorization_prompt_message = ParamCreator.create_str(
-        help_string="The message to display to tell the user to navigate to the authorization URL.",
+        help_string="The message to display to tell the user to navigate to the authorization URL",
         # default=InstalledAppFlow._DEFAULT_AUTH_PROMPT_MESSAGE,
         default="",
     )
--- pygooglehelper/static.py
+++ pygooglehelper/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.1"
+VERSION_STR = "0.0.2"
 DESCRIPTION = "help you with the google API"
 APP_NAME = "pygooglehelper"
 LOGGER_NAME = "pygooglehelper"
