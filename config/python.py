8908732c68a9f61b3204e2d2dc657bfc81db1bb6
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:11:51 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -2,40 +2,28 @@ import config.project
 
 package_name = config.project.project_name
 
-console_scripts = [
-]
-
-setup_requires = [
-]
-
 run_requires = [
     "google-api-python-client",
     "google-auth-httplib2",
     "google-auth-oauthlib",
-    'pytconf',
+    "pytconf",
 ]
 
 test_requires = [
-    'pylint',
-    'pytest',
-    'pytest-cov',
-    'pyflakes',
-    'flake8',
-    'pymakehelper',
+    "pylint",
+    "pytest",
+    "pytest-cov",
+    "pyflakes",
+    "flake8",
+    "pymakehelper",
 ]
 
 dev_requires = [
-    'pyclassifiers',
-    'pypitools',
-    'pydmt',
-    'Sphinx',
+    "pyclassifiers",
+    "pypitools",
+    "pydmt",
+    "Sphinx",
 ]
-
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {
-}
 
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
