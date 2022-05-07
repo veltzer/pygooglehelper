a854acc76f0e0f0b5e2e42c019a8d16854fddbb7
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-07 05:06:16 +0300

    better templates

--- setup.py
+++ setup.py
@@ -34,14 +34,6 @@ setuptools.setup(
     platforms=[
         'python3',
     ],
-    install_requires=[
-        'google-api-python-client',
-        'google-auth-httplib2',
-        'google-auth-oauthlib',
-        'pytconf',
-    ],
-    extras_require={
-    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -55,7 +47,5 @@ setuptools.setup(
     ],
     data_files=[
     ],
-    entry_points={"console_scripts": [
-    ]},
     python_requires=">=3.9",
 )
