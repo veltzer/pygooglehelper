2d7d5f9ad2cffa5c2e5ccd77eedbdf48f177bf8f
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 02:53:03 +0200

    new pydmt version

--- config/python.py
+++ config/python.py
@@ -1,18 +1,22 @@
-dev_requires = [
+from typing import List
+
+
+config_requies: List[str] = []
+dev_requires: List[str] = [
     "pypitools",
 ]
-install_requires = [
+install_requires: List[str] = [
     "google-api-python-client",
     "google-auth-httplib2",
     "google-auth-oauthlib",
     "pytconf",
 ]
-make_requires = [
+make_requires: List[str] = [
     "pymakehelper",
     "pydmt",
     "pyclassifiers",
 ]
-test_requires = [
+test_requires: List[str] = [
     "pylint",
     "pytest",
     "pytest-cov",
@@ -20,3 +24,4 @@ test_requires = [
     "flake8",
     "mypy",
 ]
+requires = config_requires + install_requires + make_requires + test_requires
