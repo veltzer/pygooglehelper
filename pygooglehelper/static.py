5cccf382de01a5a7b7e34c69c4bd8d7f7ae27c76
Mark Veltzer <mark.veltzer@gmail.com>
2024-04-26 03:27:45 +0300

    no commit message given

--- pygooglehelper/static.py
+++ pygooglehelper/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.5"
+VERSION_STR = "0.0.6"
 DESCRIPTION = "help you with the google API"
 APP_NAME = "pygooglehelper"
 LOGGER_NAME = "pygooglehelper"
