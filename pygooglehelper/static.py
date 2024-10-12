23d375827ab9f0c191ede3aa5dad91b39754df2f
Mark Veltzer <mark.veltzer@gmail.com>
2024-10-12 15:10:58 +0300

    new version

--- pygooglehelper/static.py
+++ pygooglehelper/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.6"
+VERSION_STR = "0.0.7"
 DESCRIPTION = "help you with the google API"
 APP_NAME = "pygooglehelper"
 LOGGER_NAME = "pygooglehelper"
