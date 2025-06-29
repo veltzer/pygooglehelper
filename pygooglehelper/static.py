85fc7a07c6ea25d046b7ce0e8f29f052efbc19e0
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 09:33:44 +0300

    fixed up deps

--- pygooglehelper/static.py
+++
@@ -1,5 +0,0 @@
-""" version which can be consumed from within the module """
-VERSION_STR = "0.0.8"
-DESCRIPTION = "help you with the google API"
-APP_NAME = "pygooglehelper"
-LOGGER_NAME = "pygooglehelper"
