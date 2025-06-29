450bca399f5b14b3027a30903d7ceb08084b361f
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-29 15:04:14 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -10,8 +10,10 @@ install_requires: list[str] = [
     "pytconf",
 ]
 build_requires: list[str] = [
-    "pymakehelper",
+    "hatch",
     "pydmt",
+    "pymakehelper",
+    "pycmdtools",
 ]
 test_requires: list[str] = [
     "pylint",
