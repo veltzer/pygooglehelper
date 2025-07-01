2fb147c0f78481f364241b89b35994692924668a
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 16:15:26 +0300

    no commit message given

--- config/python.py
+++ config/python.py
@@ -8,16 +8,6 @@ install_requires: list[str] = [
     "google-auth-oauthlib",
     "pytconf",
 ]
-build_requires: list[str] = [
-    "hatch",
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-]
-test_requires: list[str] = [
-    "pylint",
-    "pytest",
-    "mypy",
-    "ruff",
-]
+build_requires: list[str] = config.shared.PBUILD
+test_requires: list[str] = config.shared.PTEST
 requires = install_requires + build_requires + test_requires
